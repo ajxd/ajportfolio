e7772ba33ced067d129e29035d4ef6bd018a995f
Ajai Kamaraj <77797319+ajxd@users.noreply.github.com>
2024-12-22 07:49:36 -0500

    Update LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -40,7 +40,12 @@ const LandingPage: React.FC = () => {
           pointerEvents: "auto",
         }}
       >
-        <Spline scene="https://prod.spline.design/dY7qMkPj2WBtUDJH/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/g2-srm4YKZuZNVzq/scene.splinecode" 
+      />
+    </main>
+  );
+}
+ />
       </div>
     </div>
   );
